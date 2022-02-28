7a9e45450baa0e49eeb447aad05d85395beb2dfb
geangontijo <geangontijo07@gmail.com>
2022-02-27 22:58:36 -0300

    colocando variáveis no .env

--- app/Services/Http/Telegram.php
+++ app/Services/Http/Telegram.php
@@ -10,14 +10,14 @@ class Telegram
 
     public function __construct(private Client $httpClient)
     {
-        $this->botToken = '5050062512:AAFDl8kVeBf6PEJDONn0QgG3qzP8YdqfN-Y';
+        $this->botToken = env('TELEGRAM_BOT_TOKEN');
     }
 
     public function sendMessage(string $message)
     {
         $this->httpClient->get("https://api.telegram.org/bot{$this->botToken}/sendMessage", [
             'query' => [
-                'chat_id' => -616980295,
+                'chat_id' => env('TELEGRAM_CHAT_ID'),
                 'parse_mode' => 'Markdown',
                 'text' => $message
             ]
